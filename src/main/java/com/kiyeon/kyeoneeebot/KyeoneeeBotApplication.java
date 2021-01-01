48e5474fadffb931531d642299684e575dd0765b
kiyeoneee <kyeoneee@gmail.com>
2021-01-01 22:26:39 +0900

    initial commit

---
+++ src/main/java/com/kiyeon/kyeoneeebot/KyeoneeeBotApplication.java
@@ -0,0 +1,13 @@
+package com.kiyeon.kyeoneeebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyeoneeeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyeoneeeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiyeon/kyeoneeebot/KyeoneeeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyeon.kyeoneeebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyeoneeeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
